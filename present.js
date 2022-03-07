fac2c7405d68c7640c219ed2566a75daa19cbef0
Quangtv208 <100190245+Quangtv208@users.noreply.github.com>
2022-03-07 21:37:59 +0500

    Update present.js

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = 'Baby';
+var to = 'NgọcLen';
 var gift_url = 'http://www.amazon.com/gp/product/B00X4WHP5E/ref=ods_xs_ae_shurl?tag=googhydr-20&hvadid=74652194316&hvpos=1t1&hvexid=&hvnetw=g&hvrand=7560558939874219351&hvpone=&hvptwo=&hvqmt=b&hvdev=c&ref=pd_sl_2vatdqwe3p_b';
 var gift_image_url = 'dantolai.png';
 
